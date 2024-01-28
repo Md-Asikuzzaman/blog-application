2ad2c4180f96fb59a2381a39457f84292ca14470
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-01-28 17:04:50 +0600

    update code

--- components/shared/post_details/PostDetails.tsx
+++ components/shared/post_details/PostDetails.tsx
@@ -50,7 +50,7 @@ const PostDetails: NextPage<Props> = ({
           <p className='text-lg'>
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
+            humour, or randomised words which dont look even slightly
             believable. If you are going to use a passage of Lorem Ipsum, you
             need to be sure there isn't anything embarrassing hidden in the
             middle of text.
