8396dc1f826083da52d29cbe389eac87fd6d0d6f
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-01-28 17:07:59 +0600

    change some eslint rules

--- components/shared/post_details/PostDetails.tsx
+++ components/shared/post_details/PostDetails.tsx
@@ -39,6 +39,22 @@ const PostDetails: NextPage<Props> = ({
 
         <div className='flex flex-col gap-5'>
           <p className='text-lg'>{description}</p>
+
+          <p className='text-lg'>
+            It uses a dictionary of over 200 Latin words, combined with a
+            handful of model sentence structures, to generate Lorem Ipsum which
+            looks reasonable. The generated Lorem Ipsum is therefore always free
+            from repetition, injected humour, or non-characteristic words etc.
+          </p>
+
+          <p className='text-lg'>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.
+          </p>
         </div>
       </div>
     </div>
