a3d04557885a2ec75257efbe9d0132d1c0d5d7d3
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-04 13:39:47 +0600

    added random post list

--- components/shared/random/Random.tsx
+++ components/shared/random/Random.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import RandomList from './RandomList';
+import React from "react";
+import RandomList from "./RandomList";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Random = () => {
+  const { data: posts, isLoading } = useQuery<PostType[]>({
+    queryKey: ["random"],
+    queryFn: async () => {
+      const { data } = await axios.get("/api/posts/random");
+      return data;
+    },
+  });
+
   return (
-    <div className='bg-white p-5 rounded-md'>
-      <h4 className='text-lg font-semibold text-black mb-3'>Random Posts</h4>
+    <div className="bg-white p-5 rounded-md">
+      <h4 className="text-lg font-semibold text-black mb-3">Random Posts</h4>
 
-      <div className='flex flex-col gap-5'>
-        <RandomList
-          photo={'/assets/images/pc.jpg'}
-          title='After a Caribbean Hurricane'
-          date='March 17, 2023'
-        />
-
-        <RandomList
-          photo={'/assets/images/pc.jpg'}
-          title='After a Caribbean Hurricane'
-          date='Jan 17, 2024'
-        />
-
-        <RandomList
-          photo={'/assets/images/pc.jpg'}
-          title='After a Caribbean Hurricane'
-          date='Aught 17, 2023'
-        />
+      <div className="flex flex-col gap-5">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {posts?.splice(0, 5)?.map((post) => (
+              <RandomList
+                key={post.id}
+                photo={post.image}
+                title={post.title}
+                date={post.createdAt}
+              />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
--- components/shared/random/RandomList.tsx
+++ components/shared/random/RandomList.tsx
@@ -1,32 +1,33 @@
-import { NextPage } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
+import moment from "moment";
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
-import { LuClock10 } from 'react-icons/lu';
+import { LuClock10 } from "react-icons/lu";
 
 interface Props {
   photo: string;
   title: string;
-  date: string;
+  date: Date;
 }
 
 const RandomList: NextPage<Props> = ({ photo, title, date }) => {
   return (
-    <div className='flex items-center gap-3'>
+    <div className="flex items-center gap-3">
       <Image
-        className='rounded-md'
+        className="rounded-md"
         src={photo}
         height={70}
         width={90}
-        alt='img'
+        alt="img"
       />
 
       <div>
-        <Link className='hover:text-green-600 transition-colors' href='/'>
-          <h4 className='leading-5 text-base font-medi'>{title}</h4>
+        <Link className="hover:text-green-600 transition-colors" href="/">
+          <h4 className="leading-5 text-base font-medi">{title}</h4>
         </Link>
-        <span className='text-sm text-gray-500 flex items-center gap-1 mt-1'>
-          <LuClock10 size={18} /> {date}
+        <span className="text-sm text-gray-500 flex items-center gap-1 mt-1">
+          <LuClock10 size={18} /> {moment(date).fromNow()}
         </span>
       </div>
     </div>
