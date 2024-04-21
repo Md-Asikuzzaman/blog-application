16fffabe5e910b54ed584e73943ce8568f75de6f
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-04-21 13:06:09 +0600

    fix random post

--- components/shared/random/RandomList.tsx
+++ components/shared/random/RandomList.tsx
@@ -6,18 +6,17 @@ import Link from "next/link";
 import { LuClock10 } from "react-icons/lu";
 
 interface Props {
-  id: string;
-  photo: string;
-  title: string;
-  date: Date;
+  post: PostType;
 }
 
-const RandomList: NextPage<Props> = ({ id, photo, title, date }) => {
+const RandomList: NextPage<Props> = ({ post }) => {
+  const { id, title, image, createdAt } = post;
+
   return (
     <div className="flex items-center gap-3">
       <Image
         className="rounded-md"
-        src={photo}
+        src={image}
         height={70}
         width={90}
         alt="img"
@@ -31,7 +30,7 @@ const RandomList: NextPage<Props> = ({ id, photo, title, date }) => {
           <h4 className="leading-5 text-base font-medi">{title}</h4>
         </Link>
         <span className="text-sm text-gray-500 flex items-center gap-1 mt-1">
-          <LuClock10 size={18} /> {moment(date).fromNow()}
+          <LuClock10 size={18} /> {moment(createdAt).fromNow()}
         </span>
       </div>
     </div>
