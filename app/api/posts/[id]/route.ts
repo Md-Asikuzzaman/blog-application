130bf12bc1520bbae376653f14e00a2de2b5da51
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-05-20 12:09:59 +0600

    fix: get single post api

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,14 +1,40 @@
-import prisma from '@/app/database';
-import { NextResponse } from 'next/server';
+import prisma from "@/app/database";
+import { NextResponse } from "next/server";
 
+interface ApiResponse {
+  message?: string;
+  post?: ApiPostType | null;
+}
+
+interface ParamsType {
+  params: {
+    id: string;
+  };
+}
+
+// [GET] post by ID
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
-) {
-  const post = await prisma.post.findUnique({
-    where: { id: params.id },
-  });
+  context: ParamsType
+): Promise<NextResponse<ApiResponse>> {
+  try {
+    const { id } = context.params;
 
-  // update logic...
-  return NextResponse.json(post, { status: 200 });
+    const post = await prisma.post.findUnique({
+      where: { id },
+      include: {
+        author: true,
+        categories: true,
+        tags: true,
+      },
+    });
+    return NextResponse.json({ post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Failed to get post.",
+      },
+      { status: 500 }
+    );
+  }
 }
