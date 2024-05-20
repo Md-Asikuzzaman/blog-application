0f106fb6c548d4ff881f8211a16243eb3cd4151f
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-05-20 12:34:21 +0600

    feat: added delete api for post

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 interface ApiResponse {
   post?: ApiPostType | null;
   updatedPost?: ApiPostType;
+  deletedPost?: ApiPostType;
   message?: string;
 }
 
@@ -41,7 +42,7 @@ export async function GET(
 }
 
 // [UPDATE] post by ID
-export async function POST(
+export async function PATCH(
   request: Request,
   context: ParamsType
 ): Promise<NextResponse<ApiResponse>> {
@@ -59,6 +60,32 @@ export async function POST(
       },
     });
     return NextResponse.json({ updatedPost }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Failed to get post.",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+// [DELETE] post by ID
+export async function DELETE(
+  request: Request,
+  context: ParamsType
+): Promise<NextResponse<ApiResponse>> {
+  try {
+    const { id } = context.params;
+
+    await prisma.post.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(
+      { message: "Post deleted successfully." },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       {
